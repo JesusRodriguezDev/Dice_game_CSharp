b59e8727506e0ce963ec43b6e0e36e4a5bc8b568
Jesus Rodriguez <jesusrodriguez7@comcast.net>
2022-08-15 22:23:44 -0600

    started on simple script

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int playerRandomNum;
+
+Random random = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Press any key to roll the dice");
+    Console.ReadKey();
+
+    playerRandomNum = random.Next(1,7);
+    Console.WriteLine("You rolled a " + playerRandomNum);
+}
+
+Console.WriteLine("Game Over!");
